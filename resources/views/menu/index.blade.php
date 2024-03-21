34f16a51091b7b7b24d7b59873e1e28efb1f9242
Daniel Kustiawan <danilkustiawan672@gmail.com>
2024-03-21 10:54:02 +0700

    "ganti nama menu"

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -9,7 +9,7 @@
 <div class="content">
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title">Menu</h3>
+            <h3 class="card-title">Menuu</h3>
 
             <div class="card-tools">
                 <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
